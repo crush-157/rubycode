49bd9419d5c6f1d3ddb99dbb99f06a16bee164d6
Linus Hakansson <linus.hakansson@oracle.com>
2018-05-15 11:31:20 +0200

    Add real app

--- app.rb
+++ app.rb
@@ -1,16 +1,36 @@
 require "socket"
-
+require 'net/http'
+require 'sequel'
 webserver = TCPServer.new(ENV['PORT'] || 8081)
+
+url = URI.parse("http://ip.jsontest.com")
+req = Net::HTTP::Get.new(url.to_s)
+res = Net::HTTP.start(url.host, url.port) {|http|
+  http.request(req)
+}
+puts res.body
+
+def db
+  @db ||= Sequel.connect(
+  'mysql2://' + ENV.fetch('MYSQLCS_CONNECT_STRING',
+                          '127.0.0.1:3306/sample_db'),
+    :user => ENV.fetch('MYSQLCS_USER_NAME','root'),
+    :password => ENV.fetch('MYSQLCS_USER_PASSWORD','password')
+  )
+end
+
+ds = db["SELECT * FROM SampleTable"]
+ds.each {|r| puts "#{r} <br/>" }
 
 responseTxt = <<-EOT
 <!DOCTYPE html>
 <html lang="en">
 <head>
         <meta charset="utf-8">
-        <title>Hello World</title>
+        <title>Fighter running!</title>
 </head>
  <body>
-    <h4>Hello World!</h4>
+    <h4>Fighter running!</h4>
 </body>
 </html>
 EOT
