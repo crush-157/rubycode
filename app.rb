468e01e0b4ccc0584f13b5a27a63ad031ae30984
Linus Hakansson <linus.hakansson@oracle.com>
2018-05-15 09:59:57 +0200

    Change to base

--- app.rb
+++ app.rb
@@ -1,18 +1,31 @@
-require 'sinatra'
-require 'httpclient'
-require 'sequel'
+require "socket"
 
-client = HTTPClient.new
-p client.get("http://ip.jsontest.com").content
+webserver = TCPServer.new(ENV['PORT'] || 8081)
 
-def db
-  @db ||= Sequel.connect(
-  'mysql2://' + ENV.fetch('MYSQLCS_CONNECT_STRING',
-                          '127.0.0.1:3306/sample_db'),
-    :user => ENV.fetch('MYSQLCS_USER_NAME','root'),
-    :password => ENV.fetch('MYSQLCS_USER_PASSWORD','password')
-  )
+responseTxt = <<-EOT
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <title>Hello World</title>
+</head>
+ <body>
+    <h4>Hello World!</h4>
+</body>
+</html>
+EOT
+
+while session = webserver.accept
+
+    request = session.gets
+    STDERR.puts request
+
+    session.print "HTTP/1.1 200\r\n" +
+        "Content-Type: text/html\r\n" +
+        "Content-Length: #{responseTxt.bytesize}\r\n" +
+        "\r\n"
+
+    session.print responseTxt
+    session.close
+
 end
-
-ds = db["SELECT * FROM SampleTable"]
-ds.each {|r| puts "#{r} <br/>" }
