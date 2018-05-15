bf0b69220b4eb5a4d023e7d54a495f70da40af3f
Linus Hakansson <linus.hakansson@oracle.com>
2018-05-15 09:37:18 +0200

    Initial code commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'httpclient'
+require 'sequel'
+
+client = HTTPClient.new
+p client.get("http://ip.jsontest.com").content
+
+def db
+  @db ||= Sequel.connect(
+  'mysql2://' + ENV.fetch('MYSQLCS_CONNECT_STRING',
+                          '127.0.0.1:3306/sample_db'),
+    :user => ENV.fetch('MYSQLCS_USER_NAME','root'),
+    :password => ENV.fetch('MYSQLCS_USER_PASSWORD','password')
+  )
+end
+
+ds = db["SELECT * FROM SampleTable"]
+ds.each {|r| puts "#{r} <br/>" }
